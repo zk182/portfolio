671a26e9a9e6902e9e557340f22eaa747f04581e
Nicolas Battaglia <nicobattmdq@gmail.com>
2026-05-08 20:49:04 -0300

    perf(globals): reduce LCP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
 export const metadata: Metadata = {
 	title: 'Nicolas Battaglia',
@@ -15,7 +15,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" style={{ colorScheme: 'light dark' }}>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
