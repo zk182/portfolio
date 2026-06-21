d31b312cc42354c02d39a58383af11f3562b7fcf
Nicolas Battaglia <nicobattmdq@gmail.com>
2026-06-20 23:40:28 -0300

    chore(bio): some changes II

--- src/app/components/Biography/bio.tsx
+++ src/app/components/Biography/bio.tsx
@@ -5,26 +5,22 @@ export default function Bio() {
 		<div className="mt-16 leading-7 text-gray-900 text-lg dark:text-gray-100">
 			<p>
 				I like Javascript, Go & lately been diving into Rust 🦀. Working at{' '}
-				<CustomLink href="https://modo.com.ar/">Modo</CustomLink> building a{' '}
-				<CustomLink href="https://www.linkedin.com/pulse/transformando-el-transporte-p%C3%BAblico-con-vqr-en-as%C3%AD-la-di-loreto-bpc7f/?trackingId=Yr0G9Xs1QZCFa4MBnf1GRg%3D%3D">
-					public transport solution
-				</CustomLink>{' '}
-				from scratch used all over Argentina. Mostly, a backend guy. Reading
-				about{' '}
+				<CustomLink href="https://cashea.app/">Cashea</CustomLink>. Mostly,
+				a backend guy. Reading about{' '}
 				<CustomLink href="https://blog.levelupcoding.com/">
 					software design patterns
 				</CustomLink>{' '}
 				right now
 			</p>{' '}
 			<p className="mt-16">
-				My cv
+				My cv{' '}
 				<CustomLink href="/nicolas-battaglia-cv.pdf" download>
 					here
 				</CustomLink>
 				.
 			</p>
 			<p className="mt-16">
-				Want to deep dive into my work?{' '}
+				Want to deep dive into my work? Heres my
 				<CustomLink href="https://nicobattcv.vercel.app/">
 					portfolio
 				</CustomLink>
